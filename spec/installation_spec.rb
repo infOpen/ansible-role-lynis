74f56145d5554b9e4c5e6cedc15b4e7b9c8fbc90
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-05-27 06:47:21 +0200

    Manage git installation

--- spec/installation_spec.rb
+++ spec/installation_spec.rb
@@ -6,17 +6,28 @@ end
 
 describe 'lynis Ansible role' do
 
-    # Declare variables
-    packages = Array[]
-
-    if ['debian', 'ubuntu'].include?(os[:family])
-        packages = Array[ 'apt' ]
+    # Installation dir should be owned by root
+    describe file('/var/lib/lynis') do
+        it { should exist }
+        it { should be_directory }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
     end
 
-    it 'install role packages' do
-        packages.each do |pkg_name|
-            expect(package(pkg_name)).to be_installed
-        end
+    # Reports directory should exists and owned by root
+    describe file('/var/log/lynis-reports') do
+        it { should exist }
+        it { should be_directory }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+    end
+
+    # Crontab file should be defined and executed by root
+    describe file('/etc/cron.d/lynis') do
+        it { should exist }
+        it { should be_file }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        its(:content) { should match /\*\s+root/ }
     end
 end
-
